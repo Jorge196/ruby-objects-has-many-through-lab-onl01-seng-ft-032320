ff3d94851a3b5b1df54a085a7e1e0a743d310e79
Jorge Ramirez <joramirez96@gmail.com>
2020-04-08 02:39:25 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -18,8 +18,8 @@ class Doctor
   end
 
   def appointments
-    Appointment.all.select do |appointment|
-      appointment.doctor == self
+    @appointments.collect do |appointment|
+      appointment.patient 
     end
   end
 
