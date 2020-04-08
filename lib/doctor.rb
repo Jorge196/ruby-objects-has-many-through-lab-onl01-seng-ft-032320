6e81127efe3a52e4e8a858d41df999f95464e912
Jorge Ramirez <joramirez96@gmail.com>
2020-04-08 02:09:07 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -19,7 +19,7 @@ class Doctor
 
   def appointments
     Appointment.all.select do |appointment|
-    doctor_who.appointment == self
+    doctor_who.appointment.include(appointment) 
     end
   end
 
